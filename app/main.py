18a4befe0fc85ff2c5bfa280e1e9d398c6bd131d
Alexander <ja.gaslacma@unsm.edu.pe>
2025-11-16 00:40:09 -0500

    unicorn

--- app/main.py
+++ app/main.py
@@ -332,3 +332,8 @@ def obtener_especies():
     conn.close()
 
     return rows
+
+if __name__ == "__main__":
+    import uvicorn
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("app.main:app", host="0.0.0.0", port=port)
